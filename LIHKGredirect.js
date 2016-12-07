126243c41c25e892c76d4624ce86c3035eeb255e
Kaden <luiwaiki56@gmail.com>
2016-12-07 19:38:44 +0800

    Auto update

--- LIHKGredirect.js
+++ LIHKGredirect.js
@@ -3,6 +3,7 @@
 // @namespace   Kaden
 // @description Auto redirect to web lighkg.
 // @include     https://lihkg.com/thread/*
+// @downloadURL https://raw.githubusercontent.com/l2003201/LIHKGredirect/master/LIHKGredirect.js
 // @version     0.0.1
 // ==/UserScript==
 
