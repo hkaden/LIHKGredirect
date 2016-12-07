2fbdd968823966ae1ddbf924af8006f1a0b65544
Kaden <luiwaiki56@gmail.com>
2016-12-07 23:14:34 +0800

    Update LIHKGredirect.js

--- LIHKGredirect.js
+++ LIHKGredirect.js
@@ -4,7 +4,8 @@
 // @description Auto redirect to web lighkg.
 // @include     https://lihkg.com/thread/*
 // @downloadURL https://raw.githubusercontent.com/l2003201/LIHKGredirect/master/LIHKGredirect.js
-// @version     0.0.1
+// @run-at      document-start
+// @version     0.0.2
 // ==/UserScript==
 
 (function() {
